ad21d1dd5488ee1a13cd11270e0c3e616536b47e
Natarajaveerapandian Thiripuranathan <tnvpandian@gmail.com>
2018-09-23 09:49:17 -0400

    Added changes for Signup form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
-
+import { WelcomeComponent } from './welcome/welcome.component';
+import { AppRoutingModule } from './app-router.module';
+import { FitnessAppMaterialModule } from './material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
-    LoginComponent
+    LoginComponent,
+    WelcomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FitnessAppMaterialModule,
+    FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
+  signUpForm:FormGroup = new FormGroup(
+   { userName: new FormControl('', [Validators.email, Validators.required]) ,
+    passWord: new FormControl('', [Validators.minLength(6)]) ,
+    userDob: new FormControl('', [Validators.required]),
+    termsChecked: new FormControl('', [Validators.requiredTrue] )
+  }
+  );
   constructor() { }
 
   ngOnInit() {
   }
 
+  processForm() {
+    console.log(' Inside Submit!!! ', this.signUpForm );
+  }
+  getMaxDOB() {
+    let currentYear = (new Date ()).getFullYear() - 18;
+    let maxAllowedDob= new Date ();
+    return new Date( currentYear, maxAllowedDob.getMonth(), maxAllowedDob.getDate());
+  }
 }
