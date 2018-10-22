cf82c45883af32433d534ded4ad22c253fbdb820
tnvpandian <tnvpandian@gmail.com>
2018-10-22 11:14:13 +0530

    Added sorting & filtering on table

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -15,19 +15,22 @@ import {
     MatSelectModule,
     MatProgressSpinnerModule,
     MatDialogModule,
-    MatTableModule
+    MatTableModule,
+    MatSortModule
 } from "@angular/material";
 
 @NgModule({
     imports: [MatButtonModule, MatFormFieldModule, MatInputModule, MatDatepickerModule,
         MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule,
         MatIconModule, MatListModule, MatTabsModule, MatCardModule,
-        MatSelectModule, MatProgressSpinnerModule, MatDialogModule,MatTableModule
+        MatSelectModule, MatProgressSpinnerModule, MatDialogModule,MatTableModule,
+        MatSortModule
     ],
     exports: [MatButtonModule, MatFormFieldModule, MatInputModule, MatDatepickerModule,
         MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule,
         MatIconModule, MatListModule, MatTabsModule, MatCardModule,
-        MatSelectModule, MatProgressSpinnerModule, MatDialogModule,MatTableModule
+        MatSelectModule, MatProgressSpinnerModule, MatDialogModule,MatTableModule,
+        MatSortModule
     ]
 })
 export class FitnessAppMaterialModule {
--- src/app/training/past-trainings/past-trainings.component.ts
+++ src/app/training/past-trainings/past-trainings.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { TrainingService } from '../training.service';
-import { MatTableDataSource } from '@angular/material';
+import { MatTableDataSource, MatSort } from '@angular/material';
 import { Exercise } from '../exercise.model';
 
 @Component({
@@ -8,10 +8,11 @@ import { Exercise } from '../exercise.model';
   templateUrl: './past-trainings.component.html',
   styleUrls: ['./past-trainings.component.css']
 })
-export class PastTrainingsComponent implements OnInit {
+export class PastTrainingsComponent implements OnInit, AfterViewInit {
   
   displayedColumns:string [] = [];
   exercises = new MatTableDataSource<Exercise>();
+  @ViewChild(MatSort) tableSort : MatSort;
 
   constructor(private trainingService: TrainingService) {
     
@@ -20,6 +21,16 @@ export class PastTrainingsComponent implements OnInit {
   ngOnInit() {
     this.displayedColumns = ['date','name','duration','calories','state'];
     this.exercises.data = this.trainingService.getCompletedOrCancelledTrainings();
+ 
   }
+ngAfterViewInit(): void {
+  //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+  //Add 'implements AfterViewInit' to the class.
+  this.exercises.sort = this.tableSort;
+}
+
+doFilter(filterValue: string) {
+this.exercises.filter = filterValue.trim().toLowerCase();
+}
 
 }
